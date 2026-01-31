ba322e5e54befac52fe345a276f794bc8b9c7e2e
🦇Tapioca com café🦇 <andersonherminio16@gmail.com>
2026-01-31 14:37:17 -0300

    Add files via upload

---
+++ codigo 3.py
@@ -0,0 +1,9 @@
+#primeiro vem a escolha dos numeros que vão ser somados
+a = int( input('escolha um numero: '))
+
+b = int( input('escolha um numero: '))
+
+#após colocar os numeros o codigo irá fazer a soma e entregar o resultado 
+c = a + b
+
+print('o resultado é:', c)
