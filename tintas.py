09c35a2d52a3e0e4ad83437ee9d676ba65568dae
🦇Tapioca com café🦇 <andersonherminio16@gmail.com>
2026-02-07 14:12:42 -0300

    Add files via upload

---
+++ tintas.py
@@ -0,0 +1,11 @@
+#definindo as variaveis:
+l = float(input('defina a largura: '))
+al = float(input('defina a altura: '))
+
+#fazendo os calculos:
+ar =  l * al
+at = ar / 2
+
+#imprimindo na tela os resultados:
+print('a área é de: {}m²'.format(ar))
+print('a quantidade de tinta para pintar essa parede é: {} litros '.format(at))
